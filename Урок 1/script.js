3f709ab01b30a22f8d09890566b9890b56e3549b
Дмитрий Комаров <dima@MacBook-Air-Dmitrij.local>
2019-01-06 20:16:57 +0100

    To jest pierwszy commit

---
+++ Урок 1/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0])
+
+// alert("Hello World!")
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = prompt("Есть ли Вам 18?", "Да");
+
+// console.log(answer);
+
+// console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isCheked = false,
+    isClose = false;
+
+console.log(isCheked || !isClose);
