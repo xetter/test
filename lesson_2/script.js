15b85fddad9e7842f183c197430286dba0c95e83
Dymek <dymrad@gmail.com>
2019-01-07 00:05:19 +0100

    Project w.2

---
+++ lesson_2/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let num = 50;
+// while (num < 55) {
+//    console.log(num);
+//    num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue
+    }
+    console.log(i)
+}
--- project/script.js
+++ project/script.js
@@ -12,12 +12,17 @@ let appData = {
     savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
-    a2 = prompt("Во сколько обойдется?"),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
-    a4 = prompt("Во сколько обойдется?");
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?",'');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+    if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 50) {
+        appData.expenses[a] = b;
+    } else {
+        
+    }
+    
+}
 
 alert(appData.budjet / 30);
