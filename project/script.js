13a8034c1780228d7196427ff180e5a3e6b87fe5
Dymek <dymrad@gmail.com>
2019-01-16 22:31:10 +0100

    Project w.3

--- project/script.js
+++ project/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
@@ -18,11 +18,24 @@ for (let i = 0; i < 2; i++) {
 
     if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
         && a != '' && b != '' && a.length < 50) {
+            console.log("done");
         appData.expenses[a] = b;
     } else {
-        
+        i = i - 1;
     }
     
 }
 
-alert(appData.budjet / 30);
+appData.moneyPerDay = appData.budjet / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
